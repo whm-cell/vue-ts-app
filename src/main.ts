452c083b1b147ab7c3c2728c86a5b16cc3815a88
王海明 <fighting282367079@gmail.com>
2023-02-25 12:31:48 +0800

    初始化项目+引入elementplus

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,22 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import * as ElementPlusIconsVue from "@element-plus/icons-vue";
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+import zhCn from "element-plus/dist/locale/zh-cn.mjs";
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App);
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component);
+}
+
+app
+  .use(ElementPlus, {
+    locale: zhCn,
+  })
+  .use(store)
+  .use(router)
+  .mount("#app");
