6a8d426d6addc620c9d5ae342205e078016e0a45
王海明 <fighting282367079@gmail.com>
2023-02-28 21:12:32 +0800

    暂存头部布局

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,11 +1,27 @@
 <template>
   <div>
-    home hello world
-
-    <router-view></router-view>
+    <el-container>
+      <el-header>
+        <home-header></home-header>
+      </el-header>
+      <el-container>
+        <el-aside>
+          <home-aside></home-aside>
+        </el-aside>
+        <el-main>
+          <home-breadcrumb></home-breadcrumb>
+          <home-main></home-main>
+        </el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import HomeAside from "./components/HomeAside.vue";
+import HomeHeader from "./components/HomeHeader.vue";
+import HomeMain from "./components/HomeMain.vue";
+import HomeBreadcrumb from "./components/HomeBreadcrumb .vue";
+</script>
 
 <style scoped lang="scss"></style>
---
+++ src/views/Home/components/HomeAside.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>home aside</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss"></style>
