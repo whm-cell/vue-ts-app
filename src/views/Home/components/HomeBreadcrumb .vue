b54d87b2536b8b1317fb40d42b1b0847cc63cd7d
王海明 <fighting282367079@gmail.com>
2023-03-01 15:17:11 +0800

    完成动态面包屑功能

--- src/views/Home/components/HomeBreadcrumb .vue
+++ src/views/Home/components/HomeBreadcrumb .vue
@@ -1,13 +1,27 @@
 <template>
   <div>
     <el-breadcrumb separator="/">
-      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-      <el-breadcrumb-item :to="{ path: '/' }">安保处</el-breadcrumb-item>
+      <el-breadcrumb-item
+        v-for="item in breadcrumbs"
+        :key="item.name"
+        :to="{ path: item.path }"
+      >
+        {{ item.meta?.title }}
+      </el-breadcrumb-item>
     </el-breadcrumb>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useRoute } from "vue-router";
+import { computed } from "vue";
+
+// 获取到当前路由（面包屑： 只需要获取当前的路由信息就可以了）
+const route = useRoute();
+
+// 计算属性： 当路由发生变化时，route.matched会重新计算
+const breadcrumbs = computed(() => route.matched);
+</script>
 
 <style scoped lang="scss">
 // 面包屑样式
