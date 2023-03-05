0047bc974c5a71f5936584e668d11f11bcaf5b97
王海明 <fighting282367079@gmail.com>
2023-03-05 12:09:04 +0800

    联动和初始数据完成。 这里使用了ts的枚举和类型限定 。

--- src/views/Sign/Sign.vue
+++ src/views/Sign/Sign.vue
@@ -2,17 +2,22 @@
   <div>
     <el-descriptions border direction="vertical" :column="9">
       <el-descriptions-item label="月份">{{ month }}月</el-descriptions-item>
-      <el-descriptions-item label="正常出勤">kooriookami</el-descriptions-item>
-      <el-descriptions-item label="Username">kooriookami</el-descriptions-item>
-      <el-descriptions-item label="Username">kooriookami</el-descriptions-item>
-      <el-descriptions-item label="Username">kooriookami</el-descriptions-item>
-      <el-descriptions-item label="Username">kooriookami</el-descriptions-item>
-      <el-descriptions-item label="Username">kooriookami</el-descriptions-item>
+      <el-descriptions-item
+        v-for="(value, key) in DetailKey"
+        :key="key"
+        :label="value"
+      >
+        {{ detailValue[key] }}
+      </el-descriptions-item>
       <el-descriptions-item label="操作">
-        <el-button type="primary" plain size="small"> 查看详情</el-button>
+        <el-button type="primary" @click="handleToException" plain size="small">
+          查看详情</el-button
+        >
       </el-descriptions-item>
       <el-descriptions-item label="考勤状态">
-        <el-tag type="danger" size="small"> 异常</el-tag>
+        <el-tag :type="detailState.type" size="small">
+          {{ detailState.text }}</el-tag
+        >
       </el-descriptions-item>
     </el-descriptions>
 
@@ -21,7 +26,7 @@
         <el-button type="primary" size="small">在线签到</el-button>
         <el-space>
           <el-button plain>2022年</el-button>
-          <el-select v-model="month" size="small">
+          <el-select v-model="month" @change="handleChange" size="small">
             <el-option
               v-for="item in 12"
               :key="item"
@@ -38,14 +43,49 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref, reactive } from "vue";
+// 引入编程式路由
+import { useRouter } from "vue-router";
 
+const router = useRouter();
 /*  v-model 必须接响应式数据，比如由ref修饰的数据 */
 const value = ref(new Date());
 
 // 获取年和月
 const year = value.value.getFullYear();
 const month = ref(value.value.getMonth() + 1);
+
+enum DetailKey {
+  normal = "正常出勤",
+  absent = "缺勤",
+  miss = "漏打卡",
+  late = "迟到",
+  leave = "早退",
+  lateAndEarly = "迟到早退",
+}
+
+const detailState = reactive({
+  // 断言的意思：告诉ts，这个值是success或者danger
+  type: "success" as "success" | "danger",
+  text: "正常" as "正常" | "异常",
+});
+
+const detailValue = reactive({
+  normal: 0,
+  absent: 0,
+  miss: 0,
+  late: 0,
+  leave: 0,
+  lateAndEarly: 0,
+});
+
+const handleChange = () => {
+  value.value = new Date(year, month.value - 1);
+};
+
+const handleToException = () => {
+  router.push("/exception");
+};
 </script>
 
 <style scoped lang="scss">
